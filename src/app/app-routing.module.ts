2abf5186aea8d115cc1177d584d013beb0f15de3
Harveen G <harveen096@revature.net>
2022-09-23 09:53:40 -0700

    small insignificant changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogInPageComponent } from './log-in-page/log-in-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 
-const routes: Routes = [{path: '',component: LogInPageComponent },{path: 'signIn',component: SignInPageComponent } ];
+const routes: Routes = [{path: '',component: LogInPageComponent },
+{path: 'signIn',component: SignInPageComponent } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
